cccab79f54f7b312f98a73fdf9a6ed0208ecfd03
Emma Makinson <emmavioletmakinson@gmail.com>
2018-02-27 12:21:32 +0000

    Adds styling and banner

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,7 @@ class ContactsController < ApplicationController
     lead_email = params.permit(:email)[:email]
     LeadMailer.email(lead_email).deliver
 
+    flash[:notice] = "Thank you! We'll be in touch shortly."
     redirect_to root_path
   end
 end
